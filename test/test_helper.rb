bfeede7281df2b21d59a48ea45bba02c2515b89f
Oleg <oleg@khabarov.ca>
2018-01-22 12:29:54 -0800

    gonna start combing through tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,7 +14,11 @@ Rails.backtrace_cleaner.remove_silencers!
 class ActionView::TestCase
 
   def setup_test_fixture
-    @user = User.new(email: 'steve@example.com', password: 'secret', comments: 'my comment')
+    @user = User.new(
+      email:    "steve@example.com",
+      password: "secret",
+      comments: "my comment"
+    )
     @builder = BootstrapForm::FormBuilder.new(:user, @user, self, {})
     @horizontal_builder = BootstrapForm::FormBuilder.new(:user, @user, self, {
       layout:       :horizontal,
