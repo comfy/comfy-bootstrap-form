362bbd302c13b59dcea53ad1e9a7ed2c7fd48505
Oleg <oleg@khabarov.ca>
2018-01-31 11:53:38 -0800

    adding default tests for all other field helpers

--- test/form_builder_field_helpers_test.rb
+++ test/form_builder_field_helpers_test.rb
@@ -17,11 +17,6 @@ class FormBuilderFieldHelpersTest < ActionView::TestCase
     assert_xml_equal expected, actual
   end
 
-  # TODO: add all field helper and test default output
-
-
-  # TODO: move these to test specific internal methods. Below are for the `draw_label`
-
   def test_text_field_label_text
     actual = @builder.text_field(:email, bootstrap: {label: {text: "Custom Label"}})
     expected = <<-HTML
@@ -55,7 +50,6 @@ class FormBuilderFieldHelpersTest < ActionView::TestCase
     assert_xml_equal expected, actual
   end
 
-  # TODO: move to control rendering test file
   def test_text_field_control_css_class
     actual = @builder.text_field(:email, bootstrap: {control: {class: "custom_class"}})
     expected = <<-HTML
@@ -67,9 +61,6 @@ class FormBuilderFieldHelpersTest < ActionView::TestCase
     assert_xml_equal expected, actual
   end
 
-
-
-
   def test_text_field_help_text
     actual = @builder.text_field(:email, bootstrap: {help: "help text"})
     expected = <<-HTML
@@ -77,6 +68,160 @@ class FormBuilderFieldHelpersTest < ActionView::TestCase
         <label for="user_email">Email</label>
         <input class="form-control" type="text" name="user[email]" id="user_email" />
         <small class="form-text text-muted">help text</small>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_color_field
+    actual = @builder.color_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="color" name="user[test]" id="user_test" value="#000000"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_date_field
+    actual = @builder.date_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="date" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_datetime_field
+    actual = @builder.datetime_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="datetime-local" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_email_field
+    actual = @builder.email_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="email" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_month_field
+    actual = @builder.month_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="month" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_number_field
+    actual = @builder.number_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="number" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_password_field
+    actual = @builder.password_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="password" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_phone_field
+    actual = @builder.phone_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="tel" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_range_field
+    actual = @builder.range_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="range" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_search_field
+    actual = @builder.search_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="search" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_text_area
+    actual = @builder.text_area(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <textarea class="form-control" name="user[test]" id="user_test"/></textarea>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_time_field
+    actual = @builder.time_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="time" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_url_field
+    actual = @builder.url_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="url" name="user[test]" id="user_test"/>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_week_field
+    actual = @builder.week_field(:test)
+    expected = <<-HTML
+      <div class="form-group">
+        <label for="user_test">Test</label>
+        <input class="form-control" type="week" name="user[test]" id="user_test"/>
       </div>
     HTML
     assert_xml_equal expected, actual
