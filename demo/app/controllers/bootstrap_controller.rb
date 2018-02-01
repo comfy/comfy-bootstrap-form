d30a6a1e7adb36e1b9f2e261996112c463c79d09
Oleg <oleg@khabarov.ca>
2018-01-31 20:54:14 -0800

    fixing up demo

--- demo/app/controllers/bootstrap_controller.rb
+++ demo/app/controllers/bootstrap_controller.rb
@@ -1,16 +1,11 @@
 class BootstrapController < ApplicationController
 
   def form
-    @collection = [
-      Address.new(id: 1, street: 'Foo'),
-      Address.new(id: 2, street: 'Bar')
-    ]
-
-    @user = User.new(comments: "Read only string")
+    @user = User.new(test: "Read only string")
 
     @user_with_error = User.new
     @user_with_error.errors.add(:email)
-    @user_with_error.errors.add(:misc)
+    @user_with_error.errors.add(:test)
   end
 
 end
