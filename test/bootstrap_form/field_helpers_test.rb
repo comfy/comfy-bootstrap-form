c3565e2aed67236a46adb5c5777d644d8622b412
Oleg <oleg@khabarov.ca>
2018-02-01 22:44:35 -0800

    can define label options now

--- test/bootstrap_form/field_helpers_test.rb
+++ test/bootstrap_form/field_helpers_test.rb
@@ -419,6 +419,41 @@ class FieldHelpersTest < ActionView::TestCase
     assert_xml_equal expected, actual
   end
 
+  def test_radio_buttons_custom_label
+    actual = @builder.radio_buttons(:test, ["a", "b"], bootstrap: {label: {text: "Custom"}})
+    expected = <<-HTML
+      <fieldset class="form-group">
+        <legend class="col-form-label">Custom</legend>
+        <div class="form-check">
+          <input class="form-check-input" id="user_test_a" name="user[test]" type="radio" value="a"/>
+          <label class="form-check-label" for="user_test_a">A</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" id="user_test_b" name="user[test]" type="radio" value="b"/>
+          <label class="form-check-label" for="user_test_b">B</label>
+        </div>
+      </fieldset>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_radio_buttons_no_label
+    actual = @builder.radio_buttons(:test, ["a", "b"], bootstrap: {label: {hide: true}})
+    expected = <<-HTML
+      <fieldset class="form-group">
+        <div class="form-check">
+          <input class="form-check-input" id="user_test_a" name="user[test]" type="radio" value="a"/>
+          <label class="form-check-label" for="user_test_a">A</label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" id="user_test_b" name="user[test]" type="radio" value="b"/>
+          <label class="form-check-label" for="user_test_b">B</label>
+        </div>
+      </fieldset>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
   def test_check_boxes
     actual = @builder.check_boxes(:test, ["a", "b"])
     expected = <<-HTML
@@ -429,42 +464,6 @@ class FieldHelpersTest < ActionView::TestCase
           <label class="form-check-label" for="user_test_a">A</label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" id="user_test_b" name="user[test][]" type="checkbox" value="b"/>
-          <label class="form-check-label" for="user_test_b">B</label>
-        </div>
-      </fieldset>
-    HTML
-    assert_xml_equal expected, actual
-  end
-
-  def test_check_boxes_with_labels
-    actual = @builder.check_boxes(:test, [["a", "Label A"], ["b", "Label B"]])
-    expected = <<-HTML
-      <fieldset class="form-group">
-        <legend class="col-form-label">Test</legend>
-        <div class="form-check">
-          <input class="form-check-input" id="user_test_a" name="user[test][]" type="checkbox" value="a"/>
-          <label class="form-check-label" for="user_test_a">Label A</label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" id="user_test_b" name="user[test][]" type="checkbox" value="b"/>
-          <label class="form-check-label" for="user_test_b">Label B</label>
-        </div>
-      </fieldset>
-    HTML
-    assert_xml_equal expected, actual
-  end
-
-  def test_check_boxes_inline
-    actual = @builder.check_boxes(:test, ["a", "b"], bootstrap: {inline: true})
-    expected = <<-HTML
-      <fieldset class="form-group">
-        <legend class="col-form-label">Test</legend>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" id="user_test_a" name="user[test][]" type="checkbox" value="a"/>
-          <label class="form-check-label" for="user_test_a">A</label>
-        </div>
-        <div class="form-check form-check-inline">
           <input class="form-check-input" id="user_test_b" name="user[test][]" type="checkbox" value="b"/>
           <label class="form-check-label" for="user_test_b">B</label>
         </div>
