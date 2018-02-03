16365a03d10662eeb40cad9a3f1cb3b09febc3c6
Oleg <oleg@khabarov.ca>
2018-02-03 15:35:43 -0800

    adding a config test

--- test/bootstrap_form/form_builder_test.rb
+++ test/bootstrap_form/form_builder_test.rb
@@ -7,8 +7,9 @@ class FormBuilderTest < ActionView::TestCase
 
     assert_equal "default",       builder.bootstrap.layout
     assert_equal "col-sm-2",      builder.bootstrap.label_col_class
-    assert_equal "col-sm-10",      builder.bootstrap.control_col_class
-    assert_equal "text-sm-right",  builder.bootstrap.label_align_class
+    assert_equal "col-sm-10",     builder.bootstrap.control_col_class
+    assert_equal "text-sm-right", builder.bootstrap.label_align_class
+    assert_equal "mr-sm-2",       builder.bootstrap.inline_margin_class
 
     refute builder.bootstrap.horizontal?
 
@@ -19,17 +20,19 @@ class FormBuilderTest < ActionView::TestCase
     builder = BootstrapForm::FormBuilder.new(nil, nil, self, {
       skip_default_ids: true,
       bootstrap: {
-        layout:             "horizontal",
-        label_col_class:    "col-md-2",
-        control_col_class:  "col-md-10",
-        label_align_class:  "text-md-right"
+        layout:               "horizontal",
+        label_col_class:      "col-md-2",
+        control_col_class:    "col-md-10",
+        label_align_class:    "text-md-right",
+        inline_margin_class:  "mr-sm-2 mb-sm-2"
       }
     })
 
-    assert_equal "horizontal",    builder.bootstrap.layout
-    assert_equal "col-md-2",      builder.bootstrap.label_col_class
-    assert_equal "col-md-10",     builder.bootstrap.control_col_class
-    assert_equal "text-md-right", builder.bootstrap.label_align_class
+    assert_equal "horizontal",      builder.bootstrap.layout
+    assert_equal "col-md-2",        builder.bootstrap.label_col_class
+    assert_equal "col-md-10",       builder.bootstrap.control_col_class
+    assert_equal "text-md-right",   builder.bootstrap.label_align_class
+    assert_equal "mr-sm-2 mb-sm-2", builder.bootstrap.inline_margin_class
 
     assert builder.bootstrap.horizontal?
 
