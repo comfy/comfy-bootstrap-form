d66f9e2cb6b57b54699869639fe8e03b6bc1c13b
Dieter Lunn <coder2000@gmail.com>
2021-01-29 12:10:02 -0600

    fix: use if instead of unless

--- lib/comfy_bootstrap_form/form_builder.rb
+++ lib/comfy_bootstrap_form/form_builder.rb
@@ -382,15 +382,15 @@ module ComfyBootstrapForm
       form_group_class = "form-group"
       form_group_class += " row"           if bootstrap.horizontal?
       form_group_class += " mr-sm-2"       if bootstrap.inline?
-      form_group_class += " form-floating" if bootstrap.floating?
+      form_group_class += " form-floating mb-2" if bootstrap.floating
 
       content_tag(:div, class: form_group_class) do
-        unless bootstrap.floating?
-          concat label
+        if bootstrap.floating
           concat control
+          concat label
         else
-          concat control
           concat label
+          concat control
         end
       end
     end
